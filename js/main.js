5735ed7c6a9f48d62d53889a34ab9ab3b2beda2a
Martin Naumann <martin@geekonaut.de>
2015-05-03 21:36:26 +0200

    Showing a vortex

--- js/main.js
+++ js/main.js
@@ -1,11 +1,25 @@
-var World = require('three-world')
+var World = require('three-world'),
+THREE = require('three')
 
 function render() {
-  // here we'll update the world on each frame
 }
 
-World.init({ renderCallback: render })
+World.init({ renderCallback: render, clearColor: 0x000022})
 
-// Things will be put into the world here
+var tunnel = new THREE.Mesh(
+  new THREE.CylinderGeometry(100, 100, 5000, 24, 24, true),
+  new THREE.MeshBasicMaterial({
+    map: THREE.ImageUtils.loadTexture('images/space.jpg', null, function(tex) {
+      tex.wrapS = tex.wrapT = THREE.RepeatWrapping
+      tex.repeat.set(5, 10)
+      tex.needsUpdate = true
+    }),
+    side: THREE.BackSide
+  })
+)
+tunnel.rotation.x = -Math.PI/2
+World.add(tunnel)
+
+World.getScene().fog = new THREE.FogExp2(0x0000022, 0.00125)
 
 World.start()
